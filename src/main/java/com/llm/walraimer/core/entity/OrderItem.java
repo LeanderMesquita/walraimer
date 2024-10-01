4f3574e4a5451591d786607ec1185773fb2a7255
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-10-01 01:18:54 -0300

    FEAT: auxiliary table for order -> product relationship

--- src/main/java/com/llm/walraimer/core/entity/OrderItem.java
+++ src/main/java/com/llm/walraimer/core/entity/OrderItem.java
@@ -1,2 +1,43 @@
-package com.llm.walraimer.core.entity;public class OrderItem {
+package com.llm.walraimer.core.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.llm.walraimer.core.entity.pks.OrderItemPk;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_order_item")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class OrderItem {
+
+    @EmbeddedId
+    private OrderItemPk id = new OrderItemPk();
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    @JsonIgnore
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public Double getSubTotal() {
+        return price * quantity;
+    }
 }
--- src/main/java/com/llm/walraimer/core/entity/pks/OrderItemPk.java
+++ src/main/java/com/llm/walraimer/core/entity/pks/OrderItemPk.java
@@ -1,2 +1,28 @@
-package com.llm.walraimer.core.entity.pks;public class OrderItemPk {
+package com.llm.walraimer.core.entity.pks;
+
+import com.llm.walraimer.core.entity.Order;
+import com.llm.walraimer.core.entity.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@EqualsAndHashCode
+public class OrderItemPk {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+
 }
