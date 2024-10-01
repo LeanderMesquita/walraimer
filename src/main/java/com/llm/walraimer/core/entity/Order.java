62a472ba4c2a50c0331cd2421e8d21a392634ff5
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-09-30 21:12:30 -0300

    FEAT: order entity

--- src/main/java/com/llm/walraimer/core/entity/Order.java
+++ src/main/java/com/llm/walraimer/core/entity/Order.java
@@ -1,2 +1,48 @@
-package com.llm.walraimer.core.entity;public class Order {
+package com.llm.walraimer.core.entity;
+
+import com.llm.walraimer.core.entity.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_sequence")
+    @SequenceGenerator(name = "order_sequence", sequenceName = "order_seq")
+    @Setter
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    @Setter
+    private Instant moment;
+
+    private Integer orderStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order(Long id, OrderStatus orderStatus, User client) {
+        this.id = id;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
 }
