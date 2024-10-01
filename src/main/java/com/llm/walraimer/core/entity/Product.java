1cf13a84d727e1b8d6e31f5f6a3eca3ed1322648
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-10-01 00:45:43 -0300

    FEAT: Product entity

--- src/main/java/com/llm/walraimer/core/entity/Product.java
+++ src/main/java/com/llm/walraimer/core/entity/Product.java
@@ -1,2 +1,31 @@
-package com.llm.walraimer.core.entity;public class Product {
+package com.llm.walraimer.core.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode
+@Table(name = "tb_product")
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_sequence")
+    @SequenceGenerator(name = "product_sequence", sequenceName = "product_sec")
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories  = new HashSet<>();
 }
